1236a04589deb287c2181904d0b078cd053937d1
Vincent <tran.vincent@outlook.com>
2022-12-06 17:08:32 -0500

    updated project files

---
+++ src/Components/ExampleComponent.jsx
@@ -0,0 +1,10 @@
+function ExampleComponent() {
+    // do stuff
+    return (
+        <div className='exampleComponent'>
+            <h1>Example Component</h1>
+        </div>
+    )
+}
+
+export default ExampleComponent;
