a9c08537388c99496e2e456fd8aedb9193f3e7b5
CreamOCorn <smileysundae3@gmail.com>
2022-12-06 22:54:56 -0500

    Tried to fix App.js so  changes showed up on the live server

--- src/App.js
+++ src/App.js
@@ -10,44 +10,33 @@ import DynamicList from './Components/DynamicList';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-<<<<<<< HEAD
   return(
-    
-    <Router>
-      <h1>I can't see this on the live server</h1>
-      <Navbar /> (//Navbar remains there always)
-      
-      (//Renders the Home page)
-      <Route path="/" exact>
-        <Home />
-      </Route>
+    <div className='App'>
+      <BrowserRouter>
+        <Router>
+          <h1>I can't see this on the live server</h1>
+          <Navbar /> (//Navbar remains there always)
+          
+          (//Renders the Home page)
+          <Route path="/" exact>
+            <Home />
+          </Route>
 
-      (//Renders the Sending Information page)
-      <Route path="/send">
-        <Send />
-      </Route>
+          (//Renders the Sending Information page)
+          <Route path="/send">
+            <Send />
+          </Route>
 
-      (//Renders the Database Accessing page)
-      <Route path="/database">
-        <Database />
-      </Route>
-
-    </Router>
-    
+          (//Renders the Database Accessing page)
+          <Route path='/' element={<DynamicList />}/>;
+          {/* <Route path="/database">
+            <Database />
+          </Route> sorry to comment this off edwin i'm just putting this thing up here as a temp*/}
+        </Router>
+      </BrowserRouter>
+      </div>
   )
 
   }
-=======
-  return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<DynamicList />}/>;
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
->>>>>>> 29c2200c8dffa157c1e4ff2e4871dc92b5e2b061
 
 export default App;
