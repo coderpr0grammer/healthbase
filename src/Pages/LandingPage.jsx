b37a9febef24b16a4d0ecdb69aa291834002b7ea
CreamOCorn <smileysundae3@gmail.com>
2022-12-07 02:56:44 -0500

    Completed the "welcome" section of the landing page

---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,22 @@
+import './LandingPage.css'
+import HomePicture from "../Assets/LandingPagePicture1200x900.png"
+
+function Home(){
+    return (
+    <div className="homeContainer">
+        <header className="homeHeader">
+            <div className="headerText">
+                <h1>Welcome to HealthBase.</h1>
+                <p>Helping medical professionals communicate.</p>
+            </div>
+            <div className="button">
+                <a className="signupButton" href="/signup">SIGN UP</a>
+                <a className="loginButton" href="/login ">LOGIN</a>
+            </div>
+        </header>
+        <img className="homePicture" src={HomePicture} />
+    </div>
+    )
+}
+
+export default Home;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './Navbar.css';
+import Logo from "../Assets/logo192.png";
 
 function Navbar(){
-    return <nav className="navbar bg-dark container">
-        <h4><Link to="/">Home</Link></h4>
-        <h4><Link to="/send">Send</Link></h4>
-        <h4><Link to="/database">Database</Link></h4>
+    return (
+    <nav className="navbar">
+        <div className="navLogo">
+            <a href='/'>
+                <img src={Logo} />
+            </a>
+        </div>
+        <ul className="navList">
+            <Link to="/">Home</Link>
+            <Link to="/send">Send</Link>
+            <Link to="/database">Database</Link>
+        </ul>
     </nav>
+    )
 }
 
 export default Navbar;
