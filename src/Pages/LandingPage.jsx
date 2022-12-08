e00b8ec35cad5e5d2337c1eb7598e286f06478bd
CreamOCorn <smileysundae3@gmail.com>
2022-12-07 21:16:58 -0500

    Added text box to the bottom of the landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -14,7 +14,19 @@ function Home(){
                 <a className="loginButton" href="/login ">LOGIN</a>
             </div>
         </header>
-        <img className="homePicture" src={HomePicture} />
+        <img className="homePicture" src={HomePicture} /> {/*DON'T PUT IT IN THE HEADER IT WILL RUIN EVERYTHING*/}
+
+
+        <section className="textSection">
+            <div classname = "textPurpose">
+                <h1>Our purpose.</h1>
+                <p>Vincent's mom works at a pharmacy and still uses a fax machine for no reason when she COULD use healthbase.</p>
+            </div>
+            <div classname = "textInfo">
+                <h1>An easier way to access patient data.</h1>
+                <p>Working with HealthBase means having easy access to all patient information over the cloud. No more waiting for doctors to fax over information, when you can access it all with the click of a button.</p>
+            </div>
+        </section>
     </div>
     )
 }
