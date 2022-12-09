1ad4b7be774d7c0cbdc6fcb046df63adf9f70ce4
EdwinNgui <edwin.nguim@gmail.com>
2022-12-08 23:41:46 -0500

    unsure

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-function Login () {
-    return (
-        <div className="loginContainer">
-            <div className="loginHeader">
-                <h1>Login to HealthBase.</h1>
-            </div>
-            <div className="loginForm">
-                <p>Please enter your identification:</p>
-                <p>Password:</p>
-            </div>
-            <div className="button">
-                <a className="loginButton" href="/login ">LOGIN</a>
-            </div>
-=======
 import "./LoginPage.css"
 
 function Login () {
@@ -25,7 +10,6 @@ function Login () {
             </div>
              
               <a className="loginSiteButton">LOGIN</a>
->>>>>>> 503c690bd926e1b78e9af7488e41f9aed979a22c
         </div>
     )
 }
