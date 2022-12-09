6376739019573449d9580af3b82819d64e24f6f3
CreamOCorn <smileysundae3@gmail.com>
2022-12-09 09:13:45 -0500

    Allowed for form submission

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,17 +1,16 @@
 import "./InputPage.css"
-import {Link} from "react-router-dom";
 
 function Login () {
     return (
         <div className="inputContainer">
             <h1 className="inputHeader">Login to HealthBase.</h1>
-            <form className="inputForm">
+            <form className="inputForm" action="/database">
                 <p>Please enter your ID:</p>
                 <input type="text" name="username" required/>  
                 <p>Password:</p>
                 <input type="password" name="password" required/>  
+                <input className="inputSiteButton">LOGIN</input>
             </form>
-              <Link to="/database" className="inputSiteButton">LOGIN</Link>
         </div>
     )
 }
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,17 +1,16 @@
 import "./InputPage.css"
-import {Link} from "react-router-dom";
 
 function Signup () {
     return (
         <div className="inputContainer">
             <h1 className="inputHeader">Sign up with HealthBase.</h1>
-            <form className="inputForm">
+            <form className="inputForm" action="/applied">
                 <p>Please enter your organization's name:</p>
                 <input type="text" name="organization" required/>  
                 <p>Please enter an email address to contact:</p>
                 <input type="email" name="emailaddress" required/>  
+                <input className="inputSiteButton">JOIN NOW</input>
             </form>
-              <Link to="/applied" className="inputSiteButton">JOIN NOW</Link>
         </div>
     )
 }
