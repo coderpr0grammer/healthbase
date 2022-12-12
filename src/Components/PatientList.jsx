38182a88f2fe14c14b912ba68c04745103405482
Vincent <tran.vincent@outlook.com>
2022-12-11 22:23:10 -0500

    updated signuppage.jsx, update addpatient.css, update patientlist.jsx

--- src/Components/PatientList.jsx
+++ src/Components/PatientList.jsx
@@ -1,22 +1,35 @@
 import {useState} from 'react';
 import AddPatient from './AddPatient';
+import axios from 'axios';
+import './PatientList.css'
 
-function PatientList({user}) {
-
+function PatientList({user, currentPatient}) {
+    const [selectedPatient, changeSelectedPatient] = useState();
     const [modalActive, setModalState] = useState(false);
 
     function openPatientModal() {
         setModalState(!modalActive);
     }
 
+    function handleClick(patient) {
+        changeSelectedPatient(patient);
+    }
+
+    async function getPatientList() {
+        let patientList = axios.get('localhost:3001/getpatients').then(
+            console.log('Succesfully retrieved ')
+        );
+
+    }
+
     return (
         <div className="listContainer">
             <div className="patientList">
+                <div className={'addPatient patientContainer'} onClick={openPatientModal}>
+                    + Add Patient
+                </div>
                 {/* {patientList} */}
             </div>
-            <button className={`addPatient`} onClick={openPatientModal}>
-                + Add Patient
-            </button>
             <AddPatient 
                 state={modalActive}
                 changeState={setModalState}
--- src/Pages/SignupPage.jsx
+++ src/Pages/SignupPage.jsx
@@ -1,17 +1,41 @@
 import "./InputPage.css"
-
+import {useState} from 'react'
 import {Link} from 'react-router-dom';
+import sha256 from 'js-sha256'
 
 function Signup() {
+    const [input, changeInput] = useState({
+        username: null,
+        password: null
+    })
+
+    const handleSubmit = event => {
+        // todo pass input into axios get req, validate login, pass login info back up
+        event.preventDefault();
+        changeInput({
+            username: sha256(input.username),
+            username: sha256(input.password)
+        })
+
+    }
+
+    const handleChange = event => {
+        const value = event.target.value;
+        changeInput({
+            ...input,
+            [event.target.name]: value 
+        });
+    }
+
     return ( 
         <div className="inputContainer">
             <h1 className="inputHeader">Sign up with HealthBase.</h1>
-            <form className="inputForm" action="/applied">
-                <p>Please enter your organization's name:</p>
-                <input className="inputTextBox" type="text" name="organization" required/>  
-                <p>Please enter an email address to contact:</p>
-                <input className="inputTextBox" type="text" name="emailaddress" required/>  
-                <input className="inputSiteButton" type="submit" value="JOIN NOW"/>
+            <form className="inputForm" action="/applied" onSubmit={handleSubmit}>
+                <p>Enter your email:</p>
+                <input className="inputTextBox" type="email" name="email" onChange={handleChange} required/>  
+                <p>Create a password:</p>
+                <input className="inputTextBox" type="password" name="password" onChange={handleChange} required/>  
+                <input className="inputSiteButton" type="submit" value="JOIN NOW" onChange={handleChange}/>
             </form>
             <Link to="/login" className="redirectLink">Already have an account? Click here to login.</Link>
         </div>
